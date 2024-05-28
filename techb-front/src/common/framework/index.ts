6f85bc26fa49e2c728d35a6e72630d364f6cba6a
chojunmo <chojunmo@chojunmoui-MacBookAir.local>
2024-05-28 13:58:25 +0900

    git upload

--- techb-front/src/common/framework/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './bs-datatable.controller';
-export * from './bs-datatable-query-page.controller';
-export * from './bs-query-form.controller';
-//export * from './bs-update-form.controller';
-export * from './bs-base.store';
-export * from './bs-datatable.store';
--- techb-front/src/common/ui/bs-modal/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './bs-modal';
-export * from './bs-modal.service';
